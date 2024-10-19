6e9fc47f5ffadc4498c21a57955d648467f97f78
action <action@github.com>
2024-10-19 05:19:38 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,13 +1,13 @@
-//Sat Oct 19 2024 05:17:07 GMT+0000 (Coordinated Universal Time)
+//Sat Oct 19 2024 05:19:38 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 (function () {})();
-function initDate() {
-  sessionStorage.setItem(START_NUM, "0");
-  sessionStorage.setItem(TOKEN, getParamValue(TOKEN));
-  sessionStorage.setItem(STR, getParamValue(STR));
-  abc();
-  var _0x14f278 = 761762 + randomNumber;
-  sessionStorage.setItem("person", _0x14f278);
-  $("#person").text(_0x14f278);
+function lj() {
+  var _0x7d0237 = sessionStorage.getItem(TOKEN),
+    _0x217ee1 = sessionStorage.getItem(START_NUM),
+    _0x36d918 = sessionStorage.getItem(STR),
+    _0x16c3d2 = ee();
+  _0x7d0237 = _0x7d0237 + "&startNumber=" + _0x217ee1 + "&str=" + _0x36d918 + "&keys=" + _0x16c3d2 + "&ds=";
+  var _0x2e5e95 = "/uaction/getArticleListggg?token=" + _0x7d0237;
+  return _0x2e5e95;
 }
